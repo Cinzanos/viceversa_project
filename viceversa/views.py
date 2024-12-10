2b75c2ae1ab014de322fb8f3f4a9b639291c0f7c
Arslan <cinzanos123@gmail.com>
2024-12-10 20:33:00 +0300

    Add reverse feature

--- viceversa/views.py
+++ viceversa/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def reverse(request):
+    # test_get_text = request.GET['usertext']
+    # print(test_get_text) # получаем в cmd текст пользователя который находится в url после usertext= 
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text}) # создадим ключ и значение чтобы получать по ключу информацию
+# с файле reverse.html
