f6ec62fa2a1431b2438b7a5f4afb7fff344b3a57
Arslan <cinzanos123@gmail.com>
2024-12-10 20:51:13 +0300

    How many words in text

--- viceversa/views.py
+++ viceversa/views.py
@@ -10,5 +10,6 @@ def reverse(request):
     # print(test_get_text) # получаем в cmd текст пользователя который находится в url после usertext= 
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text}) # создадим ключ и значение чтобы получать по ключу информацию
+    number_words = len(user_text.split())
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'numberwords':number_words}) # создадим ключ и значение чтобы получать по ключу информацию
 # с файле reverse.html
